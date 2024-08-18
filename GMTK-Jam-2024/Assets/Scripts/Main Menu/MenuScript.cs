60ade9be64b2fd936be2c5dde080c6e00ed08b1f
Steampowered85 <adamclaridge2000@gmail.com>
2024-08-18 15:52:28 +0100

    Added Scene
    HardMode Scene

--- GMTK-Jam-2024/Assets/Scripts/Main Menu/MenuScript.cs
+++ GMTK-Jam-2024/Assets/Scripts/Main Menu/MenuScript.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene("CutScene");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void PlayHardMode()
     {
-        
+        SceneManager.LoadScene("HardModeCutScene");
     }
 }
