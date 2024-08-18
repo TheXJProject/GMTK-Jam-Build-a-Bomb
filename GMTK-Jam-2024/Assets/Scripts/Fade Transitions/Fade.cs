9445b02d21319e3c093b3164667f68628a56ad8b
Steampowered85 <adamclaridge2000@gmail.com>
2024-08-18 18:10:39 +0100

    Hard Moded

---
+++ GMTK-Jam-2024/Assets/Scripts/Fade Transitions/Fade.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Fade : MonoBehaviour
+{
+    public static event Action onZeroAlpha;
+    public static event Action onOneAlpha;
+    
+    [SerializeField] private CanvasGroup myUIGroup;
+
+    [SerializeField] private bool fadeIn = false;
+    [SerializeField] private bool fadeOut = false;
+
+    [Range(0.5f, 5f)]
+    [SerializeField] private float fadeSpeed;
+
+    public void ShowUI()
+    {
+        fadeIn = true;
+    }
+    public void HideUI()
+    {
+        fadeOut = true;
+    }
+
+    private void Update()
+    {
+        if (fadeIn)
+        {
+            if (myUIGroup.alpha < 1)
+            {
+                myUIGroup.alpha += Time.deltaTime * fadeSpeed;
+                if (myUIGroup.alpha >= 1)
+                {
+                    onOneAlpha?.Invoke();
+                    fadeIn = false;
+                }
+            }
+        }
+
+        if (fadeOut)
+        {
+            if (myUIGroup.alpha > 0)
+            {
+                myUIGroup.alpha -= Time.deltaTime * fadeSpeed;
+                if (myUIGroup.alpha <= 0)
+                {
+                    onZeroAlpha?.Invoke();
+                    fadeOut = false;
+                }
+            }
+        }
+    }
+}
