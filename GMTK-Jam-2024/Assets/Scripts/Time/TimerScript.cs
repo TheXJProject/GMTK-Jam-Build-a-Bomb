be6a73cc602936befa85bac11d24494d391ef984
Steampowered85 <adamclaridge2000@gmail.com>
2024-08-19 19:49:33 +0100

    Scene Changer Changes

--- GMTK-Jam-2024/Assets/Scripts/Time/TimerScript.cs
+++ GMTK-Jam-2024/Assets/Scripts/Time/TimerScript.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class TimerScript : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        layer_controller.onTimerStart += StartTimer;
+    }
 
+    private void OnDisable()
+    {
+        layer_controller.onTimerStart -= StartTimer;
+    }
+
+    private void StartTimer()
+    {
+
+    }
 }
